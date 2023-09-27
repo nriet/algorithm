de35d66e95e6b5bfc6c4586defd7ac1435d43ffa
nriet <itzyx@vip.qq.com>
2023-09-28 01:08:12 +0800

    LANG=C.UTF-8

--- zj-cpcs/user_init.sh
+++ zj-cpcs/user_init.sh
@@ -8,15 +8,13 @@ if [ ! -d "/space/cmadaas/dpl/$1" ]; then
         echo "user exists"
         else
         useradd -d /space/cmadaas/dpl/$1 -m -s /bin/bash -u  $2 $1
-        echo "LANG=C.UTF-8" >> /space/cmadaas/dpl/$1/.bashrc
     fi
 else
     echo "/space/cmadaas/dpl/$1 目录已存在！重新授权用户$1"
         if id -u $1 >/dev/null 2>&1; then
       echo "user exists"
         else
-      useradd -d /space/cmadaas/dpl/$1 -m -s /bin/bash -u $2 $1
-      echo "LANG=C.UTF-8" >> /space/cmadaas/dpl/$1/.bashrc
+      useradd -d /space/cmadaas/dpl/$1 -m -s /bin/bash -u $2 $1 
     fi
     chown -R $1:$1 /space/cmadaas/dpl/$1
     cp /etc/skel/.bash* /space/cmadaas/dpl/$1/
