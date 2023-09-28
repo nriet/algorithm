79a05bfd86a95e84baf17337bed630d50dd8d2e1
nriet <itzyx@vip.qq.com>
2023-09-28 13:28:32 +0800

    sudo

--- zj-cpcs/user_init.sh
+++ zj-cpcs/user_init.sh
@@ -9,6 +9,7 @@ if [ ! -d "/space/cmadaas/dpl/$1" ]; then
         else
         useradd -d /space/cmadaas/dpl/$1 -m -s /bin/bash -u  $2 $1
         echo "LANG=C.UTF-8" >> /space/cmadaas/dpl/$1/.bashrc
+        usermod -aG sudo $1
     fi
 else
     echo "/space/cmadaas/dpl/$1 目录已存在！重新授权用户$1"
@@ -17,11 +18,14 @@ else
         else
       useradd -d /space/cmadaas/dpl/$1 -m -s /bin/bash -u $2 $1
       echo "LANG=C.UTF-8" >> /space/cmadaas/dpl/$1/.bashrc
+      usermod -aG sudo $1
     fi
     chown -R $1:$1 /space/cmadaas/dpl/$1
     cp /etc/skel/.bash* /space/cmadaas/dpl/$1/
 fi
 
 echo $1:$3 | chpasswd
-sed -i '$a\$1  ALL=(ALL)  NOPASSWD: ALL' /etc/sudoers
+
+
+#sed -i '$a\$1  ALL=(ALL)  NOPASSWD: ALL' /etc/sudoers
 #/usr/sbin/sshd -D
