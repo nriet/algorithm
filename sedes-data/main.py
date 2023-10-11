183e091fc9c1561b517e119fe4852a83ab50dd1d
nriet <itzyx@vip.qq.com>
2023-10-11 16:27:34 +0800

    sedes-data-2023

--- sedes-data/main.py
+++ sedes-data/main.py
@@ -27,7 +27,7 @@ else:
     yystart = int(argument[0:4])
     yylast = int(argument[0:4])
     # months = ["01",'02','03','04','05','06','07','08','09','10','11','12']
-    months = ["01",'02','03','04','05','06','07','08','09','10']
+    months = ["01",'02']
     day = 1
 
     for year in range(yystart, yylast + 1):
